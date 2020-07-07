082d5f0d3229479fbc6a09209a3dd89de958f789
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-06 21:29:30 -0300

    Conditional one

---
+++ 5 - Conditional if - 1/conditional.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int money = 20;
+
+    // if (money > 10)
+    // {
+    //     cout << "Go to watch a movie!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Sorry you don\'t have money" << endl;
+    // }
+
+    if (money >= 10)
+    {
+        cout << "Go to watch a movie!" << endl;
+    }
+    else
+    {
+        cout << "Sorry you don\'t have money" << endl;
+    }
+
+    return 0;
+}
