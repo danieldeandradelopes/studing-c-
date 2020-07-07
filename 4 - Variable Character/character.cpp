331e509102cb55ffac8042aacf8a36eb91c2ff27
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-06 21:25:08 -0300

    Lesson four

---
+++ 4 - Variable Character/character.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // char name = 'Daniel';
+    // cout << name << endl;
+
+    // sum asc of characters
+
+    // char c1 = 'a', c2 = 'b';
+    // int sum;
+    // sum = c1 + c2;
+
+    // cout << sum << endl;
+
+    // print '
+
+    char character = '\'';
+
+    cout << character << endl;
+
+    return 0;
+}
