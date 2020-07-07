27f4c002eadd39dba4901a4fbffea62850363f9e
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-07-06 21:50:57 -0300

    update switch

--- 7 - Switch/switch.cpp
+++ 7 - Switch/switch.cpp
@@ -4,23 +4,58 @@ using namespace std;
 
 int main()
 {
-    int num = 13;
+    // int num = 13;
 
-    switch (num)
+    // switch (num)
+    // {
+    // case 9:
+    //     cout << "Number 9" << endl;
+    //     break;
+    // case 10:
+    //     cout << "Number 10" << endl;
+    //     break;
+
+    // case 11:
+    //     cout << "Number 11" << endl;
+    //     break;
+
+    // default:
+    //     cout << "Number isn't in interval" << endl;
+    //     break;
+    // }
+
+    int num1, num2, result;
+    char op;
+
+    cout << "Enter with a first number: ";
+    cin >> num1;
+
+    cout << "Enter with a second number: ";
+    cin >> num2;
+
+    cout << "Selected: + - / * ";
+    cin >> op;
+
+    switch (op)
     {
-    case 9:
-        cout << "Number 9" << endl;
+    case '+':
+        result = num1 + num2;
+        cout << "Sum: " + result;
         break;
-    case 10:
-        cout << "Number 10" << endl;
+    case '-':
+        result = num1 - num2;
+        cout << "Subtraction: " + result;
         break;
-
-    case 11:
-        cout << "Number 11" << endl;
+    case '/':
+        result = num1 / num2;
+        cout << "Division: " + result;
+        break;
+    case '*':
+        result = num1 * num2;
+        cout << "Multiplication: " + result;
         break;
 
     default:
-        cout << "Number isn't in interval" << endl;
         break;
     }
 }
